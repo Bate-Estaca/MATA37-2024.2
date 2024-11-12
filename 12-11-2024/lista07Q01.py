86bffcf2f7b8cd457219d5aaf4bf99ee037c5748
Bate-Estaca <luanderson.ufba@gmail.com>
2024-11-12 18:02:01 -0300

    Código da questão 1 melhorado

--- 12-11-2024/lista07Q01.py
+++ 12-11-2024/lista07Q01.py
@@ -1,11 +1,2 @@
-n = int(input("Insira um número: "))
-
-m = 1
-
-while m<=n:
-    l = 0
-    while l < m:
-        print(m,end="\t" )
-        l += 1
-    print()
-    m += 1
+for n in range((int(input("Insira um número: "))) + 1):
+    print(f"{n}\t"*n)
