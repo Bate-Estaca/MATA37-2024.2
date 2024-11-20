7fbffa64b156dee43da1a07a40993127edae8fe5
Bate-Estaca <luanderson.ufba@gmail.com>
2024-11-20 10:47:59 -0300

    quickpatch: No arquivo lista08Q05.py o array copy estava recebendo um pointer para o parâmetro da função invés de copiar o conteúdo do parâmetro.

--- 19-11-2024/lista08Q05.py
+++ 19-11-2024/lista08Q05.py
@@ -1,12 +1,12 @@
 def has_duplicated(_list):
-    copy = _list
+    copy = _list[:]
     while len(copy) > 0:
         toSearch = copy.pop(0)
         if toSearch in copy:
             return True
     return False
 
+_list = [1,2,3,4,5,5]
 
-print(has_duplicated([1,2,3]))
-print(has_duplicated([1,2,2]))
-print(has_duplicated([3,2,3]))
+print(has_duplicated(_list))
+print(_list)
