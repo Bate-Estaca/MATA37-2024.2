ec7fc99dc95d088a38fced3702dafd3233b410e1
Bate-Estaca <luanderson.ufba@gmail.com>
2024-12-12 12:40:52 -0300

    Falta lista 11

--- 03-12-2024/lista10Q12.py
+++ 03-12-2024/lista10Q12.py
@@ -6,15 +6,51 @@ if len(S1) > 20:
     print(f"\nA string {S1} possui {len(S1)} caracteres! O tamanho máximo permitido é 20!\n")
     sleep(3)
     exit(1)
+    
+def main():
+   _exit = False
+   system("cls||clear")
+   while(not _exit):
+        drawMenu()
+        print("\n")
+        _exit = choice(input("> "))
+        input("Pressione ENTER para continuar...")
+        system("cls||clear")
+
+   return 0
 
 def drawMenu():
-    system("cls||clear")
-    print("1- Tamanho da string")
-    print("2- Comparar com outra string")
-    print("3- Concatenar com outra string")
-    print("4- Imprimir string ao reverso")
-    print("5- Contar um caracter")
-    print("6- Verificar subString")
-    print("7- Criar subString")
-drawMenu()
+   print("1- Tamanho da string")
+   print("2- Comparar com outra string")
+   print("3- Concatenar com outra string")
+   print("4- Imprimir string ao reverso")
+   print("5- Contar um caracter")
+   print("6- Verificar subString")
+   print("7- Criar subString")
+   print()
+   print("8. sair")
+   print('\n\n')
 
+def choice(expr):
+    match int(expr):
+        case 1:
+            print(f"O tamanho da string \"{S1}\" é: {len(S1)}")
+        case 2:
+            print(input("Digite a string a ser comparada: ") == S1)
+        case 3:
+            print(S1 + input("Digite a string a ser concatenada: "))
+        case 4:
+            print("".join(list(S1)[::-1]))
+        case 5:
+            _char = input("Digite o caracter a ser contado: ")
+            print(f"{_char}: ",S1.count(_char))
+        case 6:
+            print(S1.find(input("Digite a substring a ser procurada: ")) != -1)
+        case 7:
+            [start, lenght] = map(int, input("Insira o inicio e o tamanho da substring no format: INICIO TAMANHO").split())
+            print(S1[start:start+lenght])
+        case 8:
+            return True
+    return False
+
+main()
--- 10-12-2024/lista11Q05.py
+++ 10-12-2024/lista11Q05.py
@@ -1,22 +1,24 @@
-from random import randrange 
+from random import randrange ,choice
 
 primeiro_nome = ["Lucas", "João Pedro", "Maria", "Fernanda", "Luciana", "Rafael", "Igor", "Cauê", "Julia", "Roberta"]
 segundo_nome = ["Silva","Bittencourt", "Almeida", "Conceissão", "Fernandes", "de Souza", "Albuquerque", "Magalhães", "Pereira"]
+DDDs = [11,12,13,14,15,16,17,18,19,21,22,24,27,28,31,32,33,34,35,37,38,41,42,43,44,45,46,47,48,49,51,53,54,55,61,62,63,64,65,66,67,68,69,71,73,74,75,77,79,81,82,83,84,85,86,87,88,89,91,92,93,94,95,96,97,98,99]
 agenda = {}
+
 
 for n in range(int(input("Defina o número de entradas: "))):
     cpf = ""
     telefone = ""
     for m in range(11):
         cpf += str(randrange(0,10))
-    for m in range(9):
+    for m in range(8):
         telefone += str(randrange(0,10))
 
+    
     agenda[cpf]={
-            "nome":f"{primeiro_nome[randrange(0,len(primeiro_nome))]} {segundo_nome[randrange(0,len(segundo_nome))]} {segundo_nome[randrange(0,len(segundo_nome))]}",
+            "nome":f"{choice(primeiro_nome)} {choice(segundo_nome)} {choice(segundo_nome)}",
             "idade": randrange(1,120),
-            "telefone": telefone.replace(telefone[5], f"-{telefone[5]}",1)
-
+            "telefone": "+55(" + str(choice(DDDs)) + ")9" + telefone[0:4] + "-" + telefone[4:8]
             }
 
 for key in agenda:
